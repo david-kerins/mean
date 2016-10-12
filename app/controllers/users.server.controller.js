73cdb5231e7e633505dbccfda9070b399b60599b
David Kerins <dkerins@bitbox.ca>
2016-10-11 19:45:07 -0700

    Added Passport support for local and OAuth authentication

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,79 +1,95 @@
-var User = require('mongoose').model('User');
-exports.create = function(req, res, next) {
-    var user = new User(req.body);
-    user.save(function(err) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(user);
+var User = require('mongoose').model('User'),
+    passport = require('passport');
+var getErrorMessage = function(err) {
+    var message = '';
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
         }
-    });
-};
-exports.list = function(req, res, next) {
-    User.find({}, function(err, users) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(users);
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].
+                message;
         }
-    });
+    }
+    return message;
 };
-exports.read = function(req, res) {
-    res.json(req.user);
+exports.renderSignin = function(req, res, next) {
+    if (!req.user) {
+        res.render('signin', {
+            title: 'Sign-in Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    } else {
+        return res.redirect('/');
+    }
 };
-exports.test = function(req, res) {
-    User.testStatic();
-  User.testStatic(function(err, users) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(users);
-        }
-    });
+exports.renderSignup = function(req, res, next) {
+    if (!req.user) {
+        res.render('signup', {
+            title: 'Sign-up Form',
+            messages: req.flash('error')
+        });
+    } else {
+        return res.redirect('/');
+    }
 };
-exports.userByID = function(req, res, next, id) {
+exports.signup = function(req, res, next) {
+    if (!req.user) {
+        var user = new User(req.body);
+        var message = null;
+        user.provider = 'local';
+        user.save(function(err) {
+            if (err) {
+                var message = getErrorMessage(err);
+                req.flash('error', message);
+                return res.redirect('/signup');
+            }
+            req.login(user, function(err) {
+                if (err) return next(err);
+                return res.redirect('/');
+            });
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+exports.signout = function(req, res) {
+    req.logout();
+    res.redirect('/');
+};
+exports.saveOAuthUserProfile = function(req, profile, done) {
     User.findOne({
-        _id: id
+        provider: profile.provider,
+        providerId: profile.providerId
     }, function(err, user) {
         if (err) {
-            return next(err);
+            return done(err);
         } else {
-            req.user = user;
-            next();
+            if (!user) {
+                var possibleUsername = profile.username ||
+                    ((profile.email) ? profile.email.split('@')[0] : '');
+                User.findUniqueUsername(possibleUsername, null,
+                    function(availableUsername) {
+                        profile.username = availableUsername;
+                        user = new User(profile);
+                        user.save(function(err) {
+                            if (err) {
+                                var message = _this.getErrorMessage(err);
+                                req.flash('error', message);
+                                return res.redirect('/signup');
+                            }
+                            return done(err, user);
+                        });
+                    });
+            } else {
+                return done(err, user);
+            }
         }
     });
-};
-//Not working correctly...
-exports.userByUsername = function(req, res, next, loginid) {
-    User.findOneByUsername({
-        username: loginid
-    }, function(err, user) {
-        if (err) {
-            return next(err);
-        } else {
-            req.user = user;
-            console.log('The USER: ');
-            console.log(user);
-            next();
-        }
-    });
-    console.log(loginid);
-};
-exports.update = function(req, res, next) {
-    User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(user);
-        }
-    });
-};
-exports.delete = function(req, res, next) {
-    req.user.remove(function(err) {
-        if (err) {
-            return next(err);
-        } else {
-            res.json(req.user);
-        }
-    })
 };
