981d4c70d06826b5e5fad2c124745e2d4ff9a2fe
David Kerins <David.Kerins@gov.bc.ca>
2016-10-27 22:15:04 -0700

    Added debug tooling

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,14 @@ module.exports = function(grunt) {
                     ext: 'js,html',
                     watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
                 }
+            },
+            debug: {
+                script: 'server.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    ext: 'js,html',
+                    watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
+                }
             }
         },
         mochaTest: {
@@ -62,7 +70,16 @@ module.exports = function(grunt) {
                 options: {
                     logConcurrentOutput: true
                 }
+            },
+            debug: {
+                tasks: ['nodemon:debug', 'watch', 'node-inspector'],
+                options: {
+                    logConcurrentOutput: true
+                }
             }
+        },
+        'node-inspector': {
+            debug: {}
         }
     });
 
@@ -75,8 +92,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-node-inspector');
+    grunt.registerTask('default', ['env:dev', 'lint', 'concurrent:dev']);
+    grunt.registerTask('debug', ['env:dev', 'lint', 'concurrent:debug']);
 
-    grunt.registerTask('default', ['env:dev', 'lint', 'concurrent']);
     grunt.registerTask('test', ['env:test', 'mochaTest', 'karma', 'protractor']);
     grunt.registerTask('lint', ['jshint', 'csslint']);
 
